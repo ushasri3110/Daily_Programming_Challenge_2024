150b1d94fbabecead396b6115b076454a42eda41
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-09-09 10:10:32 +0530

    Add files via upload

---
+++ Sort012.java
@@ -0,0 +1,33 @@
+public class Sort012 {
+    public static void sort_numbers(int[] arr){
+        int low=0;
+        int mid=0;
+        int high=arr.length-1;
+        while (mid<=high){
+            if (arr[mid]==0){
+                int temp=arr[low];
+                arr[low]=arr[mid];
+                arr[mid]=temp;
+                low++;
+                mid++;
+            }
+            else if (arr[mid]==1){
+                mid++;
+            }
+            else{
+                int temp=arr[mid];
+                arr[mid]=arr[high];
+                arr[high]=temp;
+                mid++;
+                high--;
+            }
+        }
+    }
+    public static void main(String[] args){
+        int[] arr={0, 1, 2, 1, 0, 2, 1, 0};
+        sort_numbers(arr);
+        for (int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+}
