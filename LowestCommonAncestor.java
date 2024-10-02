69c7cf1aa3de9c5b06e50f3fa1aea1353241319f
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-10-02 22:17:57 +0530

    Day-24

---
+++ LowestCommonAncestor.java
@@ -0,0 +1,40 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+}
+public class LowestCommonAncestor {
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        return (left != null) ? left : right;
+    }
+
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+        TreeNode p = root.left; // Node 5
+        TreeNode q = root.right; // Node 1
+        TreeNode lca = lowestCommonAncestor(root, p, q);
+
+        System.out.println(lca.val);
+    }
+}
