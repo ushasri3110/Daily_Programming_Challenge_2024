d8c589ee223632fb4839fbef9a8189fb8623c310
Usha Sri Ponaka <144483101+ushasri3110@users.noreply.github.com>
2024-10-08 20:13:18 +0530

    Day-30

---
+++ CoinChangeProblem.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class CoinChangeProblem {
+    public static int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        int result = coinChange(coins, amount);
+        System.out.println(result);
+    }
+}
